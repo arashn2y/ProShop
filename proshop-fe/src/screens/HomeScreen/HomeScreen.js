6b9cc287fc7a9b8c6bc8c1ce519f444a04a487ae
Arash Nouri <arashn2y@gmail.com>
2021-07-01 09:55:35 +0200

    fetch data from server with express

--- proshop-fe/src/screens/HomeScreen/HomeScreen.js
+++ proshop-fe/src/screens/HomeScreen/HomeScreen.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 import Product from "../../components/Product/Product";
-import products from "../../products";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
   const ShowProducts = products.map(product => (
     <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
       <Product product={product} />
