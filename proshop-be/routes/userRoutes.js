cc3218a1169e8a2f9e71cb510b2f72924df6c25d
Arash Nouri <arashn2y@gmail.com>
2021-07-12 21:47:04 +0200

    routes for get and edit a user by admin added

--- proshop-be/routes/userRoutes.js
+++ proshop-be/routes/userRoutes.js
@@ -7,13 +7,19 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUserByAdmin,
 } from "../controllers/userController/userController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(registerUser).get(protect, isAdmin, getUsers);
-router.route("/:id").delete(protect, isAdmin, deleteUser);
+router
+  .route("/:id")
+  .delete(protect, isAdmin, deleteUser)
+  .get(protect, isAdmin, getUserById)
+  .put(protect, isAdmin, updateUserByAdmin);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
